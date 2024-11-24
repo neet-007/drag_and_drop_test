6555b65b1e71c0f7594a4829a71e3c7a2b936cd0
neet-007 <moayedeffect@gmail.com>
2024-11-24 22:37:33 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const div = document.getElementById("test");
+
+
+setTimeout(() => div.innerHTML = "hello", 1000);
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+export { };
+
+declare global {
+    type Test = {
+        foo: string;
+        bar: number;
+    };
+}
