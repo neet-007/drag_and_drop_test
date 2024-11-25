2807a038e05af81a37b558f58b51da7b612adf0c
neet-007 <moayedeffect@gmail.com>
2024-11-26 00:19:54 +0300

    the proof of concept is here but i need to make it a linked list

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const ARRAY_SIZE = 7;
+
 const dropDiv = document.createElement("div");
 dropDiv.style.position = "absolute";
 dropDiv.style.left = "400px";
@@ -12,9 +14,15 @@ document.addEventListener("mousemove", dragMove);
 document.addEventListener("mouseup", dragEnd);
 
 /**@type {HTMLDivElement[]}*/
-const elems = Array(7);
+const elems = Array(ARRAY_SIZE);
 
-for (let i = 0; i < 7; i++) {
+/**@type {number[][]}*/
+const groups = Array(ARRAY_SIZE).fill(Array(1)).map((_, i) => [i]);
+
+/**@type {number[]}*/
+const parents = Array(ARRAY_SIZE).fill(-1);;
+
+for (let i = 0; i < ARRAY_SIZE; i++) {
     const elem = document.createElement("div");
     elem.id = `div${i + 1}`;
     elem.style.width = "50px";
@@ -33,6 +41,22 @@ for (let i = 0; i < 7; i++) {
 let curr = null;
 let currIndex = -1;
 let currCords = { left: "0px", right: "0px" };
+
+/**
+ * @param {any[]} arr 
+ * @param {any} val 
+ */
+function removeElem(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            for (let j = i; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr.pop();
+            break;
+        }
+    }
+}
 
 /**
  *@param {DOMRectReadOnly} rect1 
@@ -69,6 +93,21 @@ function dragStart(event) {
     currIndex = Number(this.id.replace("div", "")) - 1;
 };
 
+function recurseMove(currIndex, mult, clientX, clientY, parent) {
+    console.log(currIndex, groups[currIndex]);
+    for (let i = parent === -1 ? 0 : 1; i < groups[currIndex].length; i++) {
+        const index = groups[currIndex][i];
+        const elem = elems[index];
+        const rect = elem.getBoundingClientRect();
+        elem.style.left = `${clientX - rect.width / 2}px`;
+        elem.style.top = `${clientY - rect.height / 2 + mult * 50}px`;
+        mult++;
+        if (groups[index].length > 0) {
+            recurseMove(index, mult, clientX, clientY, index);
+        }
+    }
+}
+
 /**
  * Handles the `mousedown` event for a draggable element.
  * 
@@ -76,19 +115,9 @@ function dragStart(event) {
  */
 function dragMove(event) {
     if (curr === null) return;
-    const rect = curr.getBoundingClientRect();
-    curr.style.left = `${event.clientX - (rect.width / 2)}px`;
-    curr.style.top = `${event.clientY - (rect.height / 2)}px`;
 
-    for (let i = 0; i < elems.length; i++) {
-        if (i === currIndex) continue;
-        const rect2 = elems[i].getBoundingClientRect();
-
-        if (isColiding(rect, rect2) && (i & 1) !== (currIndex & 1)) {
-            console.log("herere");
-            curr.style.opacity = "50%";
-        }
-    }
+    let mult = 0;
+    recurseMove(currIndex, mult, event.clientX, event.clientY, -1);
 }
 
 /**
@@ -107,8 +136,23 @@ function dragEnd(event) {
         const rect2 = elems[i].getBoundingClientRect();
 
         if (isColiding(rect, rect2) && (i & 1) !== (currIndex & 1)) {
+            /*
+            parents[currIndex] = i;
+            for (let j = 0; j < groups[currIndex].length; j++) {
+                const elem = elems[groups[currIndex][j]];
+                const rect2 = elem.getBoundingClientRect();
+                elem.style.top = `${rect2.bottom + i * 50}px`;
+                elem.style.left = `${rect2.left}px`;
+                groups[i].push(groups[currIndex][j]);
+            }
+            console.log(groups[i]);
+            break;
+            */
             curr.style.top = `${rect2.bottom}px`;
             curr.style.left = `${rect2.left}px`;
+            groups[i].push(currIndex);
+        } else {
+            removeElem(groups[i], currIndex);
         }
     }
 
